f6dddc53cfd6439b06f80e86e7dbc64adca3c5ed
Dan Høegh <dan.hoegh@gmail.com>
2020-03-30 13:37:42 +0200

    more code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,64 @@
 // JS by Dan Høegh
 // UCN MMD 2020
 
-// Let's create a object. It never change content so we can initialize it with the constant keyword
-const jobA = { // use "const job" not "const JobA" during lesson
+// Let's create a object. It never change content 
+// so we can initialize it with the constant keyword
+let jobA = { // use "let job" not "let JobA" during lesson
     title: 'scarer',
     description: 'scare children to extract energy',
     isActive: true,
 }
 
-// ------------- 8< ------------- 8< ------------- Cut away below after JobA is done
+// ------------- 8< ------------- Cut away below after JobA is done
 
 // Out put it in console first... then in html
 console.log(jobA);
 document.querySelector('#output').innerHTML += jobA;
 // difference in output when you output an object to html or console!
 
-// To get output in HTML from an object, we need to be specific about what we want to output
+// To get output in HTML from an object, we need 
+// to be specific about what we want to output
 document.querySelector('#output').innerHTML += jobA.title;
 document.querySelector('#output').innerHTML += jobA.description;
 document.querySelector('#output').innerHTML += jobA.isActive;
 
-// In console the output is viewable (not [object][object]), we'll get an object to click around in
+// In console the output is viewable 
+// (not [object][object]), we'll get an object to click around in
 // This is handy for figuring out where stuff is in the object
 console.log(jobA);
 console.log(jobA.title);
 console.log(jobA.description);
 console.log(jobA.isActive);
 
-// ------------- 8< ------------- 8< ------------- Cut away above after JobA is done
+// ------------- 8< ------------- Cut away above after JobA is done
 
 
-// We can add funtions to the object!
+// We can add a funtion to the object
+// (which is called a method, because it belongs to an object)
 
-const job = {
+let job = {
     title: 'scarer',
     description: 'scare children to extract energy',
     isActive: true,
     // extend job with this function
     showDetails() {
-        const status = this.isActive ? 'ongoing' : 'on pause';
-        console.log(`The position as ${this.title} means you ${this.description}. It is currently ${status}.`);
+        let status = this.isActive ? 'ongoing' : 'on pause';
+        console.log(`The position as ${job.title} means you ${job.description}. It is currently ${status}.`);
     }
 }
 
 job.showDetails();
 
-// ^^ Try changing the data in the object and see what happens to the output
+// ^^ Try changing the data in the object 
+// and see what happens to the output
 
 
-// What if we want to create another job, based on the one we already created?
+// What if we want to create another job, 
+// based on the one we already created?
 
-// We can start by copying the current job object, this uses the job object as a template
-const assistant = Object.create(job);
+// We can start by copying the current job object,
+// this uses the job object as a template
+let assistant = Object.create(job);
 // assistant.showDetails(); // <== test the new object with this line 
 
 // We use the job objects values as default values, but we can change those
@@ -72,14 +79,15 @@ console.log(assistant.test);
 keys = Object.keys(assistant);
 console.log(keys);
 
-// When we've got the keys, we can go iterate through them - you can replace 'key' with any variable name you want
+// When we've got the keys, we can go iterate through them
+// - you can replace 'key' with any variable name you want
 Object.keys(assistant).forEach(key => {
-    const value = assistant[key];
+    let value = assistant[key];
     console.log(`${key}: ${value}`);
 });
 
 // To see how many keys that are in the object we can use .length
-const length = Object.keys(assistant).length;
+let length = Object.keys(assistant).length;
 console.log(length);
 
 // Looking at values
@@ -87,12 +95,14 @@ console.log(length);
 
 // get all values from an object - Object.values()
 
-const values = Object.values(assistant);
+let values = Object.values(assistant);
 console.log(values);
 
 // Getting the key/value pais of the object into an array - Object.entries()
-const entries = Object.entries(assistant);
-console.log(entries); // The console looks different for these, this is an array, not an object
+let entries = Object.entries(assistant);
+console.log(entries); 
+// ^^ The console looks different for these,
+// ^^ this is an array, not an object
 
 // Looping through an array
 entries.forEach(entry => {
@@ -102,25 +112,38 @@ entries.forEach(entry => {
 });
 
 // Combining objects - Object.assign()
-
-const name = {
+let name = {
     firstName: 'Gordon',
     lastName: 'Freeman'
 };
-
-const details = {
+let details = {
     job: 'Theoretical physicist',
     employer: 'Black Mesa'
 };
-
-console.log(name, details);
-
-const protagonist = Object.assign(name, details);
+console.log(name);
+console.log(details);
+// combine the two into one new object
+let protagonist = Object.assign(name, details);
 console.log(protagonist);
+
+// create a function inside an object and use "this"
+// instead of the variable name of that object 
+let newProtagonist = {
+    firstName: 'Alyx',
+    lastName: 'Vance',
+    fullName: function(){ 
+        // here we create method (it's called a 
+        // method when it's attached to an object)
+        return `${this.firstName} ${this.lastName}`;
+        // In a method, "this" refers to the "owner" of the method 
+        // (in this case it is the object "newProtagonist")
+    }
+}
+console.log(newProtagonist.fullName()); 
+// ^^ try it without the parenthesis in .fullName()
 
 // locking objects is also possible - Object.freeze() & Object.seal()
 // freeze prevents modification of values and prevents adding and removing keys
-
 let antagonist = {
     name: 'GLaDOS',
     weapon: 'Sarcasm'
@@ -137,8 +160,7 @@ antagonist.purpose = 'Stopping GLaDOS from being too smart';
 console.log(antagonist);
 
 // you can test if an object is frozen by using Object.isFrozen() (boolean)
-const objectIsFrozen = Object.isFrozen(antagonist);
-
+let objectIsFrozen = Object.isFrozen(antagonist);
 console.log(`Is frozen: ${objectIsFrozen}`);
 
 // Can be used like this, so you don't try to change values in a frozen object
@@ -164,6 +186,6 @@ user = Object.seal(user);
 user.name = 'Tyler';
 user.password = 'donttalkaboutit';
 user.position = 'The guerilla terrorist of the food service industry';
-// try adding a key: Doesn't show up, can't be added due to Object.seal()
+// try adding a key: Can't be added due to Object.seal()
 user.alternatePersonality = 'Narrator'; 
 console.log(user);
